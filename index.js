949a0c55058aaaffd0eb6f02c4e93e1a09708f6c
Ananthu <ananthuks9526@example.com>
2024-06-03 20:37:40 +0530

    workflow pipeline

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const app = express()
 app.use(express.json())
 
 
+app.get('/', (req, res) => {
+    res.send('Hi...')
+})
 
 
-
-app.listen(3005,() => {
+app.listen(3005, () => {
     console.log('Server listening at http://localhost:3005')
 })
